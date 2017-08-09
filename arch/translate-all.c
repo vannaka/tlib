4caad6d0cf4e09927d24e5d4cd3542fcdf8a8bef
Mateusz Hołenko <mholenko@antmicro.com>
2017-08-09 10:40:10 +0200

    [#8661] Enable instructions counting by default.

--- arch/translate-all.c
+++ arch/translate-all.c
@@ -55,16 +55,13 @@ static inline void gen_block_header(TranslationBlock *tb)
     tcg_gen_brcondi_i32(TCG_COND_NE, flag, 0, stopflag_label);
     tcg_temp_free_i32(flag);
 
-    if(tlib_is_instruction_count_enabled())
-    {
-        icount_arg = gen_opparam_ptr + 1;
-        // at this moment this const contains magic value 88888
-        // which is replaced at gen_block_footer near the end of
-        // the block
-        TCGv_i32 instruction_count = tcg_const_i32(88888);
-        gen_helper_update_insn_count(instruction_count);
-        tcg_temp_free_i32(instruction_count);
-    }
+    icount_arg = gen_opparam_ptr + 1;
+    // at this moment this const contains magic value 88888
+    // which is replaced at gen_block_footer near the end of
+    // the block
+    TCGv_i32 instruction_count = tcg_const_i32(88888);
+    gen_helper_update_insn_count(instruction_count);
+    tcg_temp_free_i32(instruction_count);
 
     flag = tcg_temp_local_new_i32();
     tcg_gen_ld_i32(flag, cpu_env, offsetof(CPUState, tb_restart_request));
@@ -94,10 +91,7 @@ static inline void gen_block_footer(TranslationBlock *tb)
     }
     gen_set_label(stopflag_label);
     tcg_gen_exit_tb((long)tb + 2);
-    if(tlib_is_instruction_count_enabled())
-    {
-        *icount_arg = tb->icount;
-    }
+    *icount_arg = tb->icount;
     *gen_opc_ptr = INDEX_op_end;
 }
 

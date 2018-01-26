506394460311b8550fc67c6cec1cef2acce16c7b
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2018-01-26 12:33:28 +0100

    [#9578] Cleanup unused includes.

--- arch/riscv/fpu_helper.c
+++ arch/riscv/fpu_helper.c
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
-#include <stdlib.h>
 #include "cpu.h"
 
 /* convert RISC-V rounding mode to IEEE library numbers */
--- arch/riscv/translate.c
+++ arch/riscv/translate.c
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
-#include <unistd.h>
 #include "infrastructure.h"
 
 #include "cpu.h"

bd1c7ed8050e626b14ba531c91f555f877804cf0
Jakub Jatczak <jjatczak@antmicro.com>
2019-08-12 16:45:59 +0200

    [#15183] Add missing prototypes in exports.h
    Add, or move from callbacks.h, missing prototypes.
    Lines order was modified to be same as in exports.c

--- exports.h
+++ exports.h
@@ -3,37 +3,56 @@
 
 #include <stdint.h>
 
+uint32_t tlib_set_maximum_block_size(uint32_t size);
+uint32_t tlib_get_maximum_block_size(void);
+
+void gen_helpers(void);
+
 int32_t tlib_init(char *cpu_name);
 void tlib_atomic_memory_state_init(int id, uintptr_t atomic_memory_state_ptr);
 void tlib_dispose(void);
+int32_t tlib_get_executed_instructions(void);
+void tlib_reset_executed_instrucions(uint64_t val);
 void tlib_reset(void);
-
 int32_t tlib_execute(int32_t max_insns);
 void tlib_restart_translation_block(void);
+void  tlib_set_return_request(void);
 void tlib_set_paused(void);
 void tlib_clear_paused(void);
 int32_t tlib_is_wfi(void);
-int32_t tlib_get_executed_instructions(void);
-int32_t tlib_get_total_executed_instructions(void);
 
 uint32_t tlib_get_page_size(void);
 void tlib_map_range(uint64_t start_addr, uint64_t length);
 void tlib_unmap_range(uint64_t start, uint64_t end);
 uint32_t tlib_is_range_mapped(uint64_t start, uint64_t end);
+
 void tlib_invalidate_translation_blocks(uintptr_t start, uintptr_t end);
+
 void tlib_set_irq(int32_t interrupt, int32_t state);
 int32_t tlib_is_irq_set(void);
+
 void tlib_add_breakpoint(uint64_t address);
 void tlib_remove_breakpoint(uint64_t address);
-void tlib_set_translation_cache_size(uintptr_t size);
 void tlib_set_block_begin_hook_present(uint32_t val);
 
+int32_t tlib_get_total_executed_instructions(void);
+
+void tlib_set_translation_cache_size(uintptr_t size);
 void tlib_invalidate_translation_cache(void);
-uint32_t tlib_set_maximum_block_size(uint32_t size);
-uint32_t tlib_get_maximum_block_size(void);
+
 int tlib_restore_context(void);
 void* tlib_export_state(void);
 int32_t tlib_get_state_size(void);
+
+void tlib_set_chaining_enabled(uint32_t val);
+uint32_t tlib_get_chaining_enabled(void);
+
+void tlib_set_tb_cache_enabled(uint32_t val);
+uint32_t tlib_get_tb_cache_enabled(void);
+
+void tlib_set_block_finished_hook_present(uint32_t val);
+
+int32_t tlib_set_return_on_exception(int32_t value);
 void tlib_flush_page(uint64_t address);
 
 uint64_t tlib_get_register_value(int reg_number);

0aeef2171b9d32ab9be5b87e017b308856e2b678
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2020-04-20 16:44:26 +0200

    [#16484] ppc: Clean up register accessors

--- arch/ppc/cpu_registers.h
+++ arch/ppc/cpu_registers.h
@@ -1,17 +1,18 @@
 #include "cpu-defs.h"
 
+typedef enum {
 #ifdef TARGET_PPC64
-typedef enum {
-    NIP_64 = 0,
-    PC_64 = 1,
-    MSR = 2,
-    LR = 3,
-    SRR0 = 100,
-    SRR1 = 101,
-    LPCR = 200,
-} Registers;
-#else
-typedef enum {
-    NIP_32 = 0
-} Registers;
+    NIP_64 = 64,
+    MSR_64 = 65,
+    LR_64 = 67,
+    CTR_64 = 68,
+    XER_64 = 69,
 #endif
+#ifdef TARGET_PPC32
+    NIP_32 = 64,
+    MSR_32 = 65,
+    LR_32 = 67,
+    CTR_32 = 68,
+    XER_32 = 69,
+#endif
+} Registers;

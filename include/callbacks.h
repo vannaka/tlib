71a277a0f4b49073f9507c5ac4c885b7689fd68c
Peter Gielda <pgielda@antmicro.com>
2021-01-26 14:49:39 +0100

    [FIX] Remove unused callback from callbacks.h

--- include/callbacks.h
+++ include/callbacks.h
@@ -67,7 +67,6 @@ void tlib_log(enum log_level level, char *message);
 
 void tlib_on_translation_block_find_slow(uint64_t pc);
 uint32_t tlib_on_block_begin(uint64_t address, uint32_t size);
-uint32_t tlib_is_block_begin_event_enabled(void);
 void tlib_on_translation_cache_size_change(uint64_t new_size);
 void tlib_on_block_translation(uint64_t start, uint32_t size, uint32_t flags);
 extern int32_t tlib_is_on_block_translation_enabled;

9e8a0b5e84d8d840bbb651b30a618e0b6fe61959
Marcin Witkowski <mwitkowski@antmicro.com>
2021-10-05 14:24:50 +0200

    [#28910] Fix ASX, SAX, SUB16 and UQSUB instructions

--- arch/arm/op_addsub.h
+++ arch/arm/op_addsub.h
@@ -81,9 +81,11 @@ static inline uint8_t glue(unit_add8_, PFX)(uint8_t a, uint8_t b, uint16_t *ovf)
 
 static inline uint16_t glue(unit_sub16_, PFX)(uint16_t a, uint16_t b, uint16_t *ovf)
 {
-    uint32_t result = (uint32_t)a + (uint32_t)b;
-    if (result > 65535) {
-        result = 65535;
+    uint32_t result;
+    if(a < b) {
+        result = 0;
+    } else {
+        result = (uint32_t)a - (uint32_t)b;
     }
     *ovf = 0;
     return (uint16_t)result;
@@ -91,9 +93,11 @@ static inline uint16_t glue(unit_sub16_, PFX)(uint16_t a, uint16_t b, uint16_t *
 
 static inline uint8_t glue(unit_sub8_, PFX)(uint8_t a, uint8_t b, uint16_t *ovf)
 {
-    uint32_t result = (uint32_t)a + (uint32_t)b;
-    if (result > 255) {
-        result = 255;
+    uint32_t result;
+    if(a < b) {
+        result = 0;
+    } else {
+        result = (uint32_t)a - (uint32_t)b;
     }
     *ovf = 0;
     return (uint8_t)result;
@@ -347,12 +351,12 @@ uint32_t HELPER(glue(PFX, sub16))(uint32_t a, uint32_t b GE_ARG)
     uint32_t ovf_flags = 0;
 #endif
 
-    res1 = glue(unit_add16_, PFX)(a, b, &ovf);
+    res1 = glue(unit_sub16_, PFX)(a, b, &ovf);
 #ifdef ARITH_GE
     ovf_flags |= (3 * ovf);
 #endif
 
-    res2 = glue(unit_add16_, PFX)(a >> 16, b >> 16, &ovf);
+    res2 = glue(unit_sub16_, PFX)(a >> 16, b >> 16, &ovf);
 #ifdef ARITH_GE
     ovf_flags |= (3 * ovf) << 2;
 #endif
@@ -405,22 +409,22 @@ uint32_t HELPER(glue(PFX, sub8))(uint32_t a, uint32_t b GE_ARG)
 uint32_t HELPER(glue(PFX, subaddx))(uint32_t a, uint32_t b GE_ARG)
 {
     uint32_t result = 0;
-    uint16_t res1, res2, ovf;
+    uint16_t res_top, res_bottom, ovf;
 #ifdef ARITH_GE
     uint32_t ovf_flags = 0;
 #endif
 
-    res1 = glue(unit_sub16_, PFX)(a, b, &ovf);
+    res_top = glue(unit_sub16_, PFX)(a >> 16, b, &ovf);
 #ifdef ARITH_GE
     ovf_flags |= (3 * ovf);
 #endif
 
-    res2 = glue(unit_add16_, PFX)(a >> 16, b >> 16, &ovf);
+    res_bottom = glue(unit_add16_, PFX)(a, b >> 16, &ovf);
 #ifdef ARITH_GE
     ovf_flags |= (3 * ovf) << 2;
 #endif
 
-    result = (((uint32_t)res2) << 16) | res1;
+    result = (((uint32_t)res_top) << 16) | res_bottom;
 #ifdef ARITH_GE
     *(uint32_t *)gep = ovf_flags;
 #endif
@@ -431,22 +435,22 @@ uint32_t HELPER(glue(PFX, subaddx))(uint32_t a, uint32_t b GE_ARG)
 uint32_t HELPER(glue(PFX, addsubx))(uint32_t a, uint32_t b GE_ARG)
 {
     uint32_t result = 0;
-    uint16_t res1, res2, ovf;
+    uint16_t res_top, res_bottom, ovf;
 #ifdef ARITH_GE
     uint32_t ovf_flags = 0;
 #endif
 
-    res1 = glue(unit_add16_, PFX)(a, b, &ovf);
+    res_top = glue(unit_add16_, PFX)(a >> 16, b, &ovf);
 #ifdef ARITH_GE
     ovf_flags |= (3 * ovf);
 #endif
 
-    res2 = glue(unit_sub16_, PFX)(a >> 16, b >> 16, &ovf);
+    res_bottom = glue(unit_sub16_, PFX)(a, b >> 16, &ovf);
 #ifdef ARITH_GE
     ovf_flags |= (3 * ovf) << 2;
 #endif
 
-    result = (((uint32_t)res2) << 16) | res1;
+    result = (((uint32_t)res_top) << 16) | res_bottom;
 #ifdef ARITH_GE
     *(uint32_t *)gep = ovf_flags;
 #endif

28b048b87790cf2d986c8cd852c8816275a9ffdf
Adam Jeliński <ajelinski@antmicro.com>
2021-11-10 15:39:26 +0100

    [#30098] fpu: Add an appropriate license for the imported parts

--- fpu/softfloat-specialize.h
+++ fpu/softfloat-specialize.h
@@ -2,9 +2,22 @@
  * QEMU float support
  *
  * Derived from SoftFloat.
+ *
+ * This file is licensed with the SoftFloat-2b license.
+ *
+ * These specific parts of the file are licensed with the SoftFloat-2a license as
+ * they come from QEMU's fpu/softfloat-specialize.c.inc after the SoftFloat
+ * relicensing (see https://wiki.qemu.org/Features/SoftFloatRelicensing):
+ * * the 'use_signaling_nans' function,
+ * * 'if (no_signaling_nans(status)) { ... }' blocks in 'float*_is_signaling_nan'
+ *   and 'float*_is_quiet_nan' functions,
+ * * 'pickNaN' and 'pickNaNMulAdd' Xtensa implementations,
+ *
+ * The original license notices can be found below.
  */
 
 /*============================================================================
+   The "SoftFloat-2b" license:
 
    This C source fragment is part of the SoftFloat IEC/IEEE Floating-point
    Arithmetic Package, Release 2b.
@@ -32,6 +45,35 @@
    (1) the source code for the derivative work includes prominent notice that
    the work is derivative, and (2) the source code includes prominent notice with
    these four paragraphs for those parts of this code that are retained.
+
+   =============================================================================*/
+
+/*============================================================================
+   The "SoftFloat-2a" license:
+
+   This C source fragment is part of the SoftFloat IEC/IEEE Floating-point
+   Arithmetic Package, Release 2a.
+
+   Written by John R. Hauser.  This work was made possible in part by the
+   International Computer Science Institute, located at Suite 600, 1947 Center
+   Street, Berkeley, California 94704.  Funding was partially provided by the
+   National Science Foundation under grant MIP-9311980.  The original version
+   of this code was written as part of a project to build a fixed-point vector
+   processor in collaboration with the University of California at Berkeley,
+   overseen by Profs. Nelson Morgan and John Wawrzynek.  More information
+   is available through the Web page `http://HTTP.CS.Berkeley.EDU/~jhauser/
+   arithmetic/SoftFloat.html'.
+
+   THIS SOFTWARE IS DISTRIBUTED AS IS, FOR FREE.  Although reasonable effort
+   has been made to avoid it, THIS SOFTWARE MAY CONTAIN FAULTS THAT WILL AT
+   TIMES RESULT IN INCORRECT BEHAVIOR.  USE OF THIS SOFTWARE IS RESTRICTED TO
+   PERSONS AND ORGANIZATIONS WHO CAN AND WILL TAKE FULL RESPONSIBILITY FOR ANY
+   AND ALL LOSSES, COSTS, OR OTHER PROBLEMS ARISING FROM ITS USE.
+
+   Derivative works are acceptable, even for commercial purposes, so long as
+   (1) they include prominent notice that the work is derivative, and (2) they
+   include prominent notice akin to these four paragraphs for those parts of
+   this code that are retained.
 
    =============================================================================*/
 

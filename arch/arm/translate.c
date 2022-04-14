632d1d0a68d6ef6d31deedf4e11d15b454e6e0e1
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2022-04-14 12:59:53 +0200

    [#30347] ARM: Add pack_condexec helper

--- arch/arm/translate.c
+++ arch/arm/translate.c
@@ -3813,11 +3813,16 @@ static void gen_rfe(DisasContext *s, TCGv pc, TCGv cpsr)
     s->base.is_jmp = DISAS_UPDATE;
 }
 
+static inline uint32_t pack_condexec(DisasContext *s)
+{
+    return (s->condexec_cond << 4) | (s->condexec_mask >> 1);
+}
+
 static inline void
 gen_set_condexec (DisasContext *s)
 {
     if (s->condexec_mask) {
-        uint32_t val = (s->condexec_cond << 4) | (s->condexec_mask >> 1);
+        uint32_t val = pack_condexec(s);
         TCGv tmp = tcg_temp_new_i32();
         tcg_gen_movi_i32(tmp, val);
         store_cpu_field(tmp, condexec_bits);

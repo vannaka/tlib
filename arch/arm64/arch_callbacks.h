a6eb12302ca35512566fe0396a0d69705e46af51
Adam Jeliński <ajelinski@antmicro.com>
2023-03-31 09:41:42 +0200

    [#43242] arm64: arch_callbacks.h: Add a weak attribute to GIC callbacks
    A weak attribute is present in the prototypes in `arch_callbacks.c` but
    it seems that with MinGW on Windows they're only respected if present in
    the header file.

--- arch/arm64/arch_callbacks.h
+++ arch/arm64/arch_callbacks.h
@@ -9,8 +9,8 @@
 
 #include <stdint.h>
 
-uint64_t tlib_read_system_register_interrupt_cpu_interface(uint32_t offset);
-void tlib_write_system_register_interrupt_cpu_interface(uint32_t offset, uint64_t value);
+uint64_t tlib_read_system_register_interrupt_cpu_interface(uint32_t offset) __attribute__((weak));
+void tlib_write_system_register_interrupt_cpu_interface(uint32_t offset, uint64_t value) __attribute__((weak));
 
 uint64_t tlib_read_system_register_generic_timer(uint32_t offset);
 void tlib_write_system_register_generic_timer(uint32_t offset, uint64_t value);

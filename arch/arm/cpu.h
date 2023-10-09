79b420d22ecc62963b83bb1d5d51d43bd90968cb
Mateusz Karlic <mkarlic@antmicro.com>
2023-10-09 13:48:46 +0200

    [#50124] arm: system_registers: Create separate header

--- arch/arm/cpu.h
+++ arch/arm/cpu.h
@@ -38,16 +38,7 @@
 #error "Target arch can be only 32-bit or 64-bit"
 #endif
 
-/* To enable banking of coprocessor registers depending on ns-bit we
- * add a bit to distinguish between secure and non-secure cpregs in the
- * hashtable.
- */
-#define CP_REG_NS_SHIFT 29
-#define CP_REG_NS_MASK  (1 << CP_REG_NS_SHIFT)
-
-#define ENCODE_CP_REG(cp, is64, ns, crn, crm, opc1, opc2)   \
-    ((ns) << CP_REG_NS_SHIFT | ((cp) << 16) | ((is64) << 15) |   \
-     ((crn) << 11) | ((crm) << 7) | ((opc1) << 3) | (opc2))
+#include "system_registers_arm.h"
 
 #include "cpu_registers.h"
 

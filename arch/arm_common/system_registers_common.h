2b7d1b1e1f17f5583c72b4c9676ed36af9a7f309
Adam Jeliński <ajelinski@antmicro.com>
2023-11-15 09:36:32 +0100

    [#50044] arm*: sysregs: Make unhandled access log more clear

--- arch/arm_common/system_registers_common.h
+++ arch/arm_common/system_registers_common.h
@@ -224,7 +224,8 @@ static void cp_reg_add_with_key(CPUState *env, TTable *cp_regs, uint32_t *key, A
 static inline void log_unhandled_sysreg_access(const char *sysreg_name, bool is_write)
 {
     // %-6s is required to format the printf nicely
-    tlib_printf(LOG_LEVEL_WARNING, "Unhandled system instruction or register %-6s %s", is_write ? "write:" : "read:", sysreg_name);
+    tlib_printf(LOG_LEVEL_WARNING, "Unhandled system instruction or register %-6s: %s; %s", is_write ? "write:" : "read:",
+                                   sysreg_name, is_write ? "write ignored" : "returning 0");
 }
 
 static inline void log_unhandled_sysreg_read(const char *sysreg_name)
